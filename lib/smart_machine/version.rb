0a2782b2e70a5cd5d3678c609095025a62005c57
plainsource <plainsource@humanmind.me>
2022-12-05 14:06:50 +0530

    updated to version 1.1.0

--- lib/smart_machine/version.rb
+++ lib/smart_machine/version.rb
@@ -16,8 +16,8 @@ module SmartMachine
 
   module VERSION
     MAJOR = 1
-    MINOR = 0
-    TINY  = 1
+    MINOR = 1
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
