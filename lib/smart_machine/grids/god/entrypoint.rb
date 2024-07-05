9390be6884416934097905389abb3512bb8a3c9b
Healer Joy <healerjoy@humanmind.me>
2024-07-05 10:47:25 +0000

    Updated god entrypoint and god config template file.

--- lib/smart_machine/grids/god/entrypoint.rb
+++ lib/smart_machine/grids/god/entrypoint.rb
@@ -1,4 +1,22 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'fileutils'
+require 'logger'
+
+logger = Logger.new(STDOUT)
+STDOUT.sync = true
+
+# haproxy
+# system('haproxy -W -db -f /etc/haproxy/haproxy.cfg')
+
+# initial setup
+unless File.exist?('/run/initial_container_start')
+  FileUtils.touch('/run/initial_container_start')
+
+  container_name = ENV.delete('CONTAINER_NAME')
+
+  logger.info "Initial setup completed for #{container_name}."
+end
+
 exec(*ARGV)
