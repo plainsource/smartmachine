0f84302af488f7bb9fe539bfc0b305af0aaf4327
Healer Joy <healerjoy@humanmind.me>
2024-07-05 07:47:04 +0000

    Started version 1.3.0.

--- lib/smart_machine/version.rb
+++ lib/smart_machine/version.rb
@@ -16,8 +16,8 @@ module SmartMachine
 
   module VERSION
     MAJOR = 1
-    MINOR = 2
-    TINY  = 3
+    MINOR = 3
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
