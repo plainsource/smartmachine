ac05f7aed7bd6302baff738b03b58d4a2bb8b1b5
Healer Joy <healerjoy@humanmind.me>
2025-05-09 11:36:14 +0000

    added install and uninstall commands for roundcube.

--- lib/smart_machine/commands/grid_commands/roundcube.rb
+++ lib/smart_machine/commands/grid_commands/roundcube.rb
@@ -4,6 +4,30 @@ module SmartMachine
       class Roundcube < SubThor
         include Utilities
 
+        desc "install", "Install roundcube grid"
+        def install
+          inside_machine_dir do
+            with_docker_running do
+              puts "-----> Installing Roundcube"
+              machine = SmartMachine::Machine.new
+              machine.run_on_machine commands: "smartengine grid roundcube installer"
+              puts "-----> Roundcube Installation Complete"
+            end
+          end
+        end
+
+        desc "uninstall", "Uninstall roundcube grid"
+        def uninstall
+          inside_machine_dir do
+            with_docker_running do
+              puts "-----> Uninstalling Roundcube"
+              machine = SmartMachine::Machine.new
+              machine.run_on_machine commands: "smartengine grid roundcube uninstaller"
+              puts "-----> Roundcube Uninstallation Complete"
+            end
+          end
+        end
+
         desc "up", "Take UP the roundcube grid"
         option :name, type: :string
         def up
@@ -25,6 +49,24 @@ module SmartMachine
               name_option = options[:name] ? " --name=#{options[:name]}" : ""
               machine.run_on_machine commands: "smartengine grid roundcube downer#{name_option}"
             end
+          end
+        end
+
+        desc "installer", "Roundcube grid installer", hide: true
+        def installer
+          inside_engine_machine_dir do
+            name, config = SmartMachine.config.grids.roundcube.first
+            roundcube = SmartMachine::Grids::Roundcube.new(name: name.to_s)
+            roundcube.installer
+          end
+        end
+
+        desc "uninstaller", "Roundcube grid uninstaller", hide: true
+        def uninstaller
+          inside_engine_machine_dir do
+            name, config = SmartMachine.config.grids.roundcube.first
+            roundcube = SmartMachine::Grids::Roundcube.new(name: name.to_s)
+            roundcube.uninstaller
           end
         end
 
