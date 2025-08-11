7c99dcdb850671af6ef73536b579f50ae04dd666
Healer Joy <healerjoy@humanmind.me>
2025-08-11 11:18:27 +0000

    updated comment in imagesync.

--- lib/smart_machine/grids/emailer/imapsync.rb
+++ lib/smart_machine/grids/emailer/imapsync.rb
@@ -1,7 +1,7 @@
 # TODO: Add imapsync feature to emailer.
 # https://imapsync.lamiral.info
 # https://hub.docker.com/r/gilleslamiral/imapsync/
-# docker run --rm --network=networkone gilleslamiral/imapsync imapsync --host1 <hostname> --user1 <email> --password1 <password> --host2 <hostname> --user2 <email> --password2 <password> --addheader --useheader Message-Id
+# docker run --rm --network=networkone gilleslamiral/imapsync imapsync --host1 <hostname> --user1 <email> --password1 <password> --host2 <hostname> --user2 <email> --password2 <password> --ssl1 --port1 993 --ssl2 --port2 993 --automap --addheader --useheader Message-Id
 # Add the following options to delete messages and then their respective folders on the host1 server.
 # --delete1 --delete1emptyfolders --noexpungeaftereach
 # Display Message to User: For Specific IMAP Server Tips go to https://imapsync.lamiral.info/#doc
